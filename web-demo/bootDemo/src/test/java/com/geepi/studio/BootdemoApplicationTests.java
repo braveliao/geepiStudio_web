b6091f103373aea0aac149e8ec629a7628091762
liaoyong <liao__yogn@163.com>
2020-11-25 14:06:39 +0800

    add:增加 boot-demo 项目

---
+++ web-demo/bootDemo/src/test/java/com/geepi/studio/BootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geepi.studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
